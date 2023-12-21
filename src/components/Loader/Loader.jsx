446402ac5e19c6ea3fa050ba67a305789ce53e59
HannaSydoruk <hanna.sydoruk@gmail.com>
2023-12-21 11:30:35 +0200

    fixed loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,15 +5,17 @@ import { Audio } from 'react-loader-spinner';
 export default class Loader extends Component {
   render() {
     return (
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-        wrapperStyle
-        wrapperClass
-      />
+      <div className={css.wrapper}>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      </div>
     );
   }
 }
